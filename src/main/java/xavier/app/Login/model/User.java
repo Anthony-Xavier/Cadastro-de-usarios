c9ae3ffcd45bde685570b43219ac82f0c50b450d
Anthony xavier <anthonyltx12@gmail.com>
2025-09-09 11:07:30 -0300

    Conectando o java com o mysql

---
+++ src/main/java/xavier/app/Login/model/User.java
@@ -0,0 +1,24 @@
+package xavier.app.Login.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    private String email;
+
+    @NotEmpty
+    private String password;
+
+
+}
