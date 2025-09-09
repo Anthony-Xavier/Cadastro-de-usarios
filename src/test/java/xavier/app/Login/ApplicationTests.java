e18bbf9d4432ddd78db8db2fb0e6dbceb66ba5a6
Anthony xavier <anthonyltx12@gmail.com>
2025-09-09 10:42:40 -0300

    fist commit

---
+++ src/test/java/xavier/app/Login/ApplicationTests.java
@@ -0,0 +1,13 @@
+package xavier.app.Login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
